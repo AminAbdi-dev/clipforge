dff31e6a84eced7c60c274e36c053bff5f66bbf1
AMINviolin <amin.djankiller@gmail.com>
2026-06-01 14:53:45 +0330

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-
+import os
+import dj_database_url
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -89,10 +90,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.getenv(
+            "DATABASE_URL"
+        )
+    )
 }
 
 

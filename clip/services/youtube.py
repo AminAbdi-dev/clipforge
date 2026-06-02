e0aac54b4f694d1eff557984a97e3ec66f051b97
AMINviolin <amin.djankiller@gmail.com>
2026-06-02 17:11:37 +0330

    Add railway cookie support

--- clip/services/youtube.py
+++ clip/services/youtube.py
@@ -1,11 +1,8 @@
 from yt_dlp import YoutubeDL
 from pathlib import Path
-from yt_dlp.utils import DownloadError
 import uuid
 import os
 import base64
-
-
 
 MEDIA_ROOT = Path("media")
 MEDIA_ROOT.mkdir(exist_ok=True)
@@ -14,8 +11,21 @@ MEDIA_ROOT.mkdir(exist_ok=True)
 def download_video(youtube_url: str):
 
     unique_id = str(uuid.uuid4())
-
     output_path = MEDIA_ROOT / f"{unique_id}.%(ext)s"
+
+    # Create cookie file from Railway env variable
+    cookie_file = Path("/tmp/youtube_cookies.txt")
+
+    if not cookie_file.exists():
+        cookies_b64 = os.getenv("YOUTUBE_COOKIES_B64")
+
+        if cookies_b64:
+            cookie_file.write_bytes(
+                base64.b64decode(cookies_b64)
+            )
+
+    print("COOKIE ENV EXISTS:", bool(os.getenv("YOUTUBE_COOKIES_B64")))
+    print("COOKIE FILE EXISTS:", cookie_file.exists())
 
     ydl_opts = {
         "format": "best[ext=mp4]/best",
@@ -23,7 +33,7 @@ def download_video(youtube_url: str):
         "quiet": False,
         "noplaylist": True,
         "cookiefile": str(cookie_file),
-        
+
         "extractor_args": {
             "youtube": {
                 "player_client": [
@@ -41,21 +51,7 @@ def download_video(youtube_url: str):
             )
         }
     }
-    print("COOKIE FILE EXISTS:", Path("cookies/cookies.txt").exists())
 
-    cookie_file = Path("/tmp/youtube_cookies.txt")
-
-    if not cookie_file.exists():
-        cookies_b64 = os.getenv("YOUTUBE_COOKIES_B64")
-
-        if cookies_b64:
-            cookie_file.write_bytes(
-                base64.b64decode(cookies_b64)
-            )
-    
-    print("COOKIE ENV EXISTS:", bool(os.getenv("YOUTUBE_COOKIES_B64")))
-    print("COOKIE FILE EXISTS:", cookie_file.exists())
-    
     with YoutubeDL(ydl_opts) as ydl:
         try:
             info = ydl.extract_info(youtube_url, download=True)
@@ -69,5 +65,3 @@ def download_video(youtube_url: str):
         "title": info.get("title"),
         "video_path": str(downloaded_file),
     }
-
-    
